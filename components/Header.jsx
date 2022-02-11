228e8fca3852b39c10350e3579de2de761393de1
Edris poursadeghi <e.poursadehi@fanap.ir>
2022-02-11 19:45:54 +0330

    font awesome added

---
+++ components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div className="nav wrap">
+          <div className="logo">
+            <h1>SWR</h1>
+          </div>
+        </div>
+        <form>
+          <input type="text" name="" required placeholder="Enter your search" />
+          <button>serach</button>
+        </form>
+
+        <div className="row" aria-label="prev">
+          <button className="prev">
+            <i className="fas fa-chevron-left" />
+          </button>
+          <span>1</span>
+
+          <button className="next" aria-label="next">
+            <i className="fas fa-chevron-right" />
+          </button>
+          <span>1</span>
+        </div>
+
+        <div>
+          <label htmlFor="pre-page">Per Page:</label>
+          <select id="per-page">
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
