8420fbe97d59bfc457e43b2b14bab16ddbe1ab88
Edris poursadeghi <e.poursadehi@fanap.ir>
2022-02-11 19:59:56 +0330

    header style added

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,32 +8,37 @@ const Header = () => {
           <div className="logo">
             <h1>SWR</h1>
           </div>
-        </div>
-        <form>
-          <input type="text" name="" required placeholder="Enter your search" />
-          <button>serach</button>
-        </form>
 
-        <div className="row" aria-label="prev">
-          <button className="prev">
-            <i className="fas fa-chevron-left" />
-          </button>
-          <span>1</span>
+          <form>
+            <input
+              type="text"
+              name=""
+              required
+              placeholder="Enter your search"
+            />
+            <button>serach</button>
+          </form>
 
-          <button className="next" aria-label="next">
-            <i className="fas fa-chevron-right" />
-          </button>
-          <span>1</span>
-        </div>
+          <div className="row" aria-label="prev">
+            <button className="prev">
+              <i className="fas fa-chevron-left" />
+            </button>
+            <span>1</span>
 
-        <div>
-          <label htmlFor="pre-page">Per Page:</label>
-          <select id="per-page">
-            <option value="10">10</option>
-            <option value="15">15</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-          </select>
+            <button className="next" aria-label="next">
+              <i className="fas fa-chevron-right" />
+            </button>
+          </div>
+
+          <div>
+            <label htmlFor="pre-page">Per Page:</label>
+            <select id="per-page">
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+            </select>
+          </div>
         </div>
       </div>
     </header>
--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
           rel="stylesheet"
+          referrerPolicy="no-referrer"
         />
       </Head>
       <Header />
